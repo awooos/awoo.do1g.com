3afc09e04b62dacf6f8ba4f7271d7bf871059404
Ellen Marie Dash <me@duckie.co>
2020-08-25 17:20:28 -0400

    Add _bin/generate.sh

---
+++ _bin/generate.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+function cleanup() {
+    rm -rf /tmp/awooos
+}
+
+function update() {
+    src_dir="$1"
+    name="$(echo $dir | cut -d '/' -f 6)"  # /tmp/awooos/src/libraries/${name}
+    src_file="${src_dir}/README.md"
+    dest_file="libs/${name}/index.markdown"
+
+    mkdir -p "$(dirname $dest_file)"
+    printf -- "---\ntitle: ${name}\nlayout: default\n---\n\n" > $dest_file
+    cat "$src_file" >> $dest_file
+}
+
+trap cleanup EXIT
+
+cleanup  # In case it's somehow left over from before.
+git clone --recurse-submodules https://github.com/awooos/awooos.git /tmp/awooos || exit $?
+
+for dir in /tmp/awooos/src/libraries/*; do
+    update $dir
+done
+
+bundle install || exit $?
+bundle exec jekyll build || exit $?
